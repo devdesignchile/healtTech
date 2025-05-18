62c7c79b0dd9c62fbbcd264c53dc7e790b4deca8
rodri <rodrigo.chavez@alumnos.ucn.cl>
2025-05-18 17:57:22 -0400

    Merge remote-tracking branch 'origin/main'

--- HealtTech/settings.py
+++ HealtTech/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=%e8!@e=yv45t#ens3_18nxs^-xfhjo()c4fz!8art7#m!(v@q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
