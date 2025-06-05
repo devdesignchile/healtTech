0bc8a6ac19887a3f9863d51d020b0e27d30ba265
rodri <rodrigo.chavez@alumnos.ucn.cl>
2025-06-05 18:33:58 -0400

    Merge remote-tracking branch 'origin/main'

--- HealtTech/settings.py
+++ HealtTech/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=%e8!@e=yv45t#ens3_18nxs^-xfhjo()c4fz!8art7#m!(v@q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['healttechagency.com', 'www.healttechagency.com', '82.25.64.197']
+ALLOWED_HOSTS = ['healttechagency.com', 'www.healttechagency.com', '82.25.64.197']
 
 
 RENDER_EXTERNAl_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
